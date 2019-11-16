ff7dc157a93253efd90f1ba202981ac3425bd303
YURIc5 <arewane.c5@gmail.com>
2019-11-16 21:36:46 +0900

    postsテーブル作成

--- db/migrate/20191116102807_create_posts.rb
+++ db/migrate/20191116102807_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :picture, null: false
+      t.string :text, limit: 254, null: false
       t.timestamps
     end
   end
