2ddc6246be9dd309757a9e1882c4a4a4b32aefcc
YURIc5 <arewane.c5@gmail.com>
2019-11-18 17:50:06 +0900

    投稿時のバリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :picture, PictureUploader
+
+  validates :picture, presence: true
+  validates :text, presence: true,
+              length: { in: 1..75, message: "の入力は最大84文字までです"}
 end
