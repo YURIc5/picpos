d222e7a880c86194f44ad5c88677d47cbc29de04
YURIc5 <arewane.c5@gmail.com>
2019-11-19 11:48:26 +0900

    postモデルのバリデーション修正
    textは254byteまで = 84文字まで

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
 
   validates :picture, presence: true
   validates :text, presence: true,
-              length: { in: 1..75, message: "の入力は最大84文字までです"}
+              length: { in: 1..84, message: "の入力は最大84文字までです"}
 end
