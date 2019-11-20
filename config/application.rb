01107b095ed2a21d8f26eea2ebf65e0c02240932
YURIc5 <arewane.c5@gmail.com>
2019-11-20 12:09:00 +0900

    Revert "日本時間にする設定"
    This reverts commit 2ee9459dc30a1dac314786ff52a2c45473c54141.

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Picpos
   class Application < Rails::Application
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
     # Initialize configuration defaults for originally generated Rails version.
